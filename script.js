898301df4c748e24ef1b3171bfba0b3c770547a6
LuigiBSI2024 <laa10@aluno.ifnmg.edu.br>
2025-07-23 23:59:57 +0000

    Testando e configurando

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const carrinho = [];
 
+console.log(`este é o tamanho da lista: ${carrinho.length}, escopo global`);
+
 function adicionarPedido(item) {
   carrinho.push(item);
+
   alert(`${item} adicionado à sacola!`);
-  atualizarLinkZap();
+
 }
 
 function atualizarLinkZap() {
-  const texto = encodeURIComponent(`Olá! Gostaria de pedir:\n- ${carrinho.join('\n- ')}`);
-  const link = `https://wa.me/5599999999999?text=${texto}`;
-  document.getElementById("linkZap").href = link;
+  
+  if(carrinho.length === 0){
+    alert("Não é possível fazer o pedido se o carrinho estiver vazio.");
+    console.log("Teste, oi, oi");
+  }
+  
+  else{
+    const texto = encodeURIComponent(`Olá! Gostaria de pedir:\n- ${carrinho.join('\n- ')}`);
+    const link = `https://wa.me/5599999999999?text=${texto}`;
+    document.getElementById("linkZap").href = link;
+  }
+  
 }
+
+
+
+function animacaoDecorativa(){
+ // body.addbackground("Imagens/varios_lanches_na_diagonal.gif");
+}
+
+//animacaoDecorativa();
